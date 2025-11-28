4a5e6cfd367c71bdd084024ccca5854854c2dc93
Stunning Hub <60594587+stunninghub@users.noreply.github.com>
2025-11-28 23:06:46 +0530

    refined again

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import { useState } from 'react'
+import React, { useRef } from 'react'
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
+import flareImg from "./assets/flare.svg"
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { useRef } = React;
+  const container = useRef();
+
+  gsap.registerPlugin(useGSAP);
+
+  useGSAP(() => {
+
+    const flareTimeline = gsap.timeline({ repeat: -1, repeatDelay: 0.5 });
+    flareTimeline.to(".heading img", { rotate: '360deg', duration: 2 });
+    flareTimeline.to(".heading img", { rotate: '0deg', duration: 2 });
+
+
+    flareTimeline.repeat();
+    // gsap.fromTo(".heading", { opacity: 0 }, { opacity: 1, duration: 2 });
+  },
+    { scope: container }
+  );
 
   return (
     <>
-      <div className="container">
-        <h1 className="heading">Coming Soon</h1>
+      <div className="container" ref={container}>
+        <h1 className="heading">
+          <span className="heading_txt">Coming Soon</span>
+          <img src={flareImg} alt="" width={30} />
+        </h1>
 
         <p className="sub-text">
           Something elegant, powerful, and designed <br />
-            for creators is arriving shortly.
+          for creators is arriving shortly.
+          
         </p>
-
-        <em className='info-text'>- Ashwani Verma</em>
       </div>
     </>
   )
