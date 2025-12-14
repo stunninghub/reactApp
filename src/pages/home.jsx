f55d16f8dfab9ea57b9c6a89f4d7e36057129563
Stunning Hub <60594587+stunninghub@users.noreply.github.com>
2025-12-14 23:13:00 +0530

    Add UI assets, sliders, and react-slick integration
    Added multiple UI image assets and logos, integrated react-slick for sliders, and included corresponding Slick CSS files. Updated the home page to showcase new slider sections and floating images, and enhanced App.css with styles for new components. Also updated vite.config.js for allowed hosts and package.json dependencies.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,10 @@ import gsap from 'gsap';
 import SplitText from 'gsap/dist/SplitText';
 import { useGSAP } from '@gsap/react';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import Slider from "react-slick";
+import "../Slick.css";
+import "../Slick-theme.css";
+
 
 export function HomePage() {
 
@@ -22,7 +26,7 @@ export function HomePage() {
                 });
             }
         });
-        gsap.fromTo(".hero_info_box_wrapper", { opacity: 0 }, { opacity: 1, duration: 2 });
+        gsap.fromTo(".hero_info_box_wrapper, section.hero_section .container img.floating_left, section.hero_section .container img.floating_right", { opacity: 0 }, { opacity: 1, duration: 2 });
 
         // gsap.fromTo('section.showcase_spacer')
         gsap.fromTo('section.showcase_section .showcase_items',
@@ -150,6 +154,51 @@ export function HomePage() {
         }
     });
 
+    var slider_sets = {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 200,
+        slidesToShow: 3.5,
+        slidesToScroll: 1,
+        centerMode: true,
+    };
+
+
+    const pin_slider_left = {
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 15000,
+        cssEase: 'linear',
+        infinite: true,
+        arrows: false,
+        touchMove: true,
+        swipeToSlide: true,
+        swipe: true
+    }
+    const pin_slider_right = {
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 15000,
+        cssEase: 'linear',
+        infinite: true,
+        arrows: false,
+        touchMove: true,
+        swipeToSlide: true,
+        swipe: true,
+        rtl: true
+    }
+
     return (
         <>
             <section className="hero_section">
@@ -168,6 +217,8 @@ export function HomePage() {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quidem labore blanditiis non culpa libero dignissimos.</p>
                         </div>
                     </div>
+                    <img src="/static/imgs/rocket.png" className='floating_right' />
+                    <img src="/static/imgs/floating_girl.png" className='floating_left' />
                 </div>
             </section>
 
@@ -183,8 +234,9 @@ export function HomePage() {
                 </div>
             </section>
 
-            <section className='about_section'>
+            {/* <section className='about_section'>
                 <div className="container">
+                    <h2>Why Choose Us</h2>
                     <div className="about_card_wrap">
                         <div className="abc_left">
                             <h2>Modern, Creative and Intuitive</h2>
@@ -195,9 +247,9 @@ export function HomePage() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
 
-            <section className='showcase_section'>
+            {/* <section className='showcase_section'>
                 <div className="showcase_items">
                     <div className="showcase_item">
                         <img src="/static/imgs/shop.jpg" alt="" />
@@ -211,9 +263,179 @@ export function HomePage() {
                 </div>
             </section>
 
-            <section className='showcase_spacer'></section>
+            <section className='showcase_spacer'></section> */}
 
-            <section className='promise_section'>
+            {/* <section className="show_case_slider">
+                <div className="container">
+                    <h2>Creative Showcase</h2>
+                    <Slider {...slider_sets}>
+                        <div>
+                            <div className='showcase_item'>
+                                <img src="/static/imgs/apps.jpg" alt="" />
+                                <h4>Detective Pink</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore quaerat amet, ut rerum temporibus perspiciatis.</p>
+                                <a href="javascript:;">Read more</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='showcase_item'>
+                                <img src="/static/imgs/vector.jpeg" alt="" />
+                                <h4>Detective Pink</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore quaerat amet, ut rerum temporibus perspiciatis.</p>
+                                <a href="javascript:;">Read more</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='showcase_item'>
+                                <img src="/static/imgs/design.jpg" alt="" />
+                                <h4>Detective Pink</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore quaerat amet, ut rerum temporibus perspiciatis.</p>
+                                <a href="javascript:;">Read more</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='showcase_item'>
+                                <img src="/static/imgs/creative.jpg" alt="" />
+                                <h4>Detective Pink</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore quaerat amet, ut rerum temporibus perspiciatis.</p>
+                                <a href="javascript:;">Read more</a>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='showcase_item'>
+                                <img src="/static/imgs/shop.jpg" alt="" />
+                                <h4>Detective Pink</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolore quaerat amet, ut rerum temporibus perspiciatis.</p>
+                                <a href="javascript:;">Read more</a>
+                            </div>
+                        </div>
+                    </Slider>
+                </div>
+            </section> */}
+
+            <section className='pins_slider_sec'>
+                <div className="container">
+                    <div className="pins_side_content">
+                        <h2>Explore The Creative Ideas & Build <span>Unique</span></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione aut nam dolores tempore in recusandae ipsum facere aliquid rem sequi cum doloremque a ex vel odit, voluptatum porro earum placeat.</p>
+                    </div>
+                    <div className="pin_slider_row">
+                        <Slider {...pin_slider_left}>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_1.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_2.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_3.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_4.jpg" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_5.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_6.jpeg" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_7.png" alt="" />
+                            </div>
+                        </Slider>
+                        <Slider {...pin_slider_right}>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_7.png" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_6.jpeg" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_5.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_4.jpg" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_3.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_2.webp" alt="" />
+                            </div>
+                            <div className="pin_item">
+                                <img src="/static/imgs/ui/ui_1.webp" alt="" />
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </section>
+
+
+            <section className='developer_info'>
+                <div className="container">
+                    <div className="info_col_row">
+                        <div className="info_col">
+                            <p>Byond Portfolio</p>
+                            <h2>Let's know more about me</h2>
+                            <div className="global_insights">
+                                <h4>
+                                    <span className='insight_icon'></span>
+                                    Global insights
+                                </h4>
+                                <p>Hello world :)</p>
+                            </div>
+                        </div>
+                        <div className="info_col">
+                            <div className="tech_info">
+                                <h4>
+                                    <span className='tech_icon'></span>
+                                    Tech Stack
+                                </h4>
+                                <p>If you like using these tools too, we'll get along just fine.</p>
+                                <div className="tech_stack_slider">
+                                    <div className="tech_stack_slider_inner">
+                                        <img src="/static/imgs/ui/logos/figma.svg" alt="" />
+                                        <img src="/static/imgs/ui/logos/react.png" alt="" />
+                                        <img src="/static/imgs/ui/logos/wordpress.png" alt="" />
+                                        <img src="/static/imgs/ui/logos/php.png" alt="" />
+
+                                        <img src="/static/imgs/ui/logos/figma.svg" alt="" />
+                                        <img src="/static/imgs/ui/logos/react.png" alt="" />
+                                        <img src="/static/imgs/ui/logos/wordpress.png" alt="" />
+                                        <img src="/static/imgs/ui/logos/php.png" alt="" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='values_info'>
+                                <h4>
+                                    <span className='values_icon'></span>
+                                    Take a look at my values
+                                </h4>
+                                <p>Through various projects, I've acquired valuable skills.</p>
+                            </div>
+                        </div>
+                        <div className="info_col">
+                            <div className="map_location">
+                                <img src="/static/imgs/city_vector.png" alt="" />
+                                <div className="profile_icon">
+                                    <img src="/static/imgs/ui/me.png" alt="" />
+                                </div>
+                                <span className="location_text">📍 Stunning Hub, Ludhiana</span>
+                            </div>
+
+                            <div className="history_info">
+                                <h4>
+                                    <span className='history_icon'></span>
+                                    I take theme everywhere
+                                </h4>
+                                <p>I sometimes go back to it when I'm faced with a problem I can't solve.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <section className='promise_section'>
                 <div className="container">
                     <div className="prms_head">
                         <h2>Helping you transform your business</h2>
@@ -252,7 +474,20 @@ export function HomePage() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
+
+            {/* <section className='contact_section'>
+                <div className="container">
+                    <div className="chat_card">
+                        <h2>Let's Chat</h2>
+                        <div className="action_btns">
+                            <a href="javascript:;">Contact</a>
+                            <a href="javascript:;">Email Me</a>
+                        </div>
+                        <img src="/static/imgs/chat.png" alt="" className='chat_img' />
+                    </div>
+                </div>
+            </section> */}
 
             {/* <div className="pin_into_section">
                 <section className='intro_section'>
